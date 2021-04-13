b9814187b3bacfea2de5a343030cbdc2e74653e6
Jo Dickson <jogreybill@gmail.com>
2021-04-13 13:41:18 -0400

    Better handle lack of returned css by critical; split out success/error handling into helper functions

--- critical-css/index.js
+++ critical-css/index.js
@@ -2,37 +2,67 @@
 
 const criticalHelper = require('../critical-css-utils');
 
-module.exports = function(context, req) {
-  let response = {
-    status: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-      'Content-type': 'application/json'
-    },
-    body: {},
-  };
+const responseBase = {
+  status: 200,
+  headers: {
+    'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    'Content-type': 'application/json'
+  },
+  body: {}
+}
 
+
+module.exports = function(context, req) {
   const args = req.body.args || {};
   try {
     criticalHelper(
       args,
-      (err, { css }) => {
+      (err, criticalResponse) => {
         if (err) {
-          throw err;
+          returnError(context, req, err);
         } else {
-          response.body.result = css;
-          response.body.input = req;
-          context.res = response;
+          const { css } = criticalResponse;
+          returnSuccess(context, req, css);
         }
-
-        context.done();
-    });
+      });
   } catch(e) {
-    context.log(e);
-    response.status = 500;
-    response.body.error = e.message;
-    response.body.input = req;
-    context.res = response;
-    context.done();
+    returnError(context, req, e);
   }
 };
+
+
+/**
+ * Helper function that returns a successful context
+ *
+ * @param {object} context The request context passed to the function handler
+ * @param {object} req Request params passed to the function handler
+ * @param {*} result The successful result to return
+ */
+function returnSuccess(context, req, result) {
+  let response = responseBase;
+  response.body.result = result;
+  response.body.input  = req;
+
+  context.res = response;
+  context.done();
+}
+
+
+/**
+ * Helper function that returns an error context
+ *
+ * @param {object} context The request context passed to the function handler
+ * @param {object} req Request params passed to the function handler
+ * @param {object} err The Error that should be returned
+ */
+function returnError(context, req, err) {
+  context.log(err);
+
+  let response = responseBase;
+  response.status     = 500;
+  response.body.error = err.message;
+  response.body.input = req;
+
+  context.res = response;
+  context.done();
+}
