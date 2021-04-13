c7d1f7223678bb0a0fd1040a4e327d195efddf32
Jim Barnes <jimbarnesdeveloper@gmail.com>
2021-04-13 10:01:37 -0400

    Updated queue function to use shared helper

--- critical-css-queue/index.js
+++ critical-css-queue/index.js
@@ -1,89 +1,24 @@
 'use strict';
 
-const cheerio = require('cheerio');
-const critical = require('critical');
+const criticalHelper = require('../critical-css-utils');
 
 module.exports = function (context, myQueueItem) {
   const args = myQueueItem.args;
 
-  const criticalArgs = {
-    inline: false,
-    extract: false,
-    minify: false,
-    dimensions: args.dimensions,
-    ignore: {
-      atrule: ['@font-face'] // never include font-face declarations in our critical CSS
-    },
-    penthouse: {
-      timeout: 60000 // milliseconds/1 minute
-    }
-  };
+  try {
+    criticalHelper(
+      args,
+      (err, {css}) => {
+        if (err) {
+          context.error(err);
+        } else {
+          context.log(css);
+        }
 
-  criticalArgs.html = prepareHTML(args.html, args);
-
-  critical.generate(
-    criticalArgs,
-    (err, { css }) => {
-    if (err) {
-      context.error(err);
-    } else {
-      context.log(css)
-    }
-
-    context.done();
-  });
+        context.done();
+      }
+    );
+  } catch (e) {
+    context.error(e);
+  }
 };
-
-/**
- * Prepares HTML to be passed to Critical.
- *
- * @param {string} html HTML string
- * @param {object} args Arguments passed in to the request
- * @returns {string} Processed HTML
- */
- function prepareHTML(html, args) {
-  // Load document into Cheerio for easier DOM processing/traversal
-  const $ = cheerio.load(html);
-
-  // Strip elements by CSS selector in source.exclude
-  // so that Critical is forced to ignore them
-  if (args.exclude) {
-    args.exclude.forEach((ignoreRule) => {
-      const $ignoreElem = $(ignoreRule);
-      if ($ignoreElem.length) {
-        $ignoreElem.remove();
-      }
-    });
-  }
-
-  // Remove preload and noscript tags in the head
-  const $preload = $('head link[rel="preload"]');
-  if ($preload.length) {
-    $preload.remove();
-  }
-  const $noscript = $('head noscript');
-  if ($noscript.length) {
-    $noscript.remove();
-  }
-
-  // Revert any async loading stylesheets to non-async so
-  // that Critical can do its job.
-  //
-  // For the purpose of this script, just set the media attr
-  // to `screen` for all async-loaded styles.
-  const $headStyles = $('head link[rel="stylesheet"]');
-  if ($headStyles.length) {
-    $headStyles.each((i, elem) => {
-      const onload = $(elem).attr('onload');
-      if (onload) {
-        $(elem).attr('media', 'screen');
-        $(elem).attr('onload', null);
-      }
-    });
-  }
-
-  const preparedHTML = $.html();
-
-  return preparedHTML;
-}
-
