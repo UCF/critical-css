93e8d8965af3ed78c1bac0b1048cc182eed02b33
Jim Barnes <jimbarnesdeveloper@gmail.com>
2021-04-15 10:19:16 -0400

    Added logic to handle queueing requests with html and url defined

--- enqueue-job/index.js
+++ enqueue-job/index.js
@@ -1,15 +1,35 @@
 'use strict';
 
 module.exports = async function (context, req) {
-  if (req.body && req.body.args && req.body.args.html && req.body.args.dimensions) {
-    context.bindings.msg = req.body;
-    context.res = {
-      body: "Success"
-    }
-  } else {
-    context.res = {
-      status: 400,
-      body: "You must provide the html and dimensions arguments in the args field."
-    }
+  // Set our response object
+  let response = {
+    status: 200,
+    body: ''
+  };
+
+  let argError = false;
+
+  if ( ! req.body || ! req.body.args ) {
+    response.status = 400;
+    response.body = 'The request body must include the an args array with valid arguments'
+    argError = true;
+  } else if ( ! req.body.args.hasOwnProperty('html') || ! req.body.args.hasOwnProperty('url') ) {
+    response.status = 400;
+    response.body = 'The args must include either the page "html" or a "url" to retrieve the html from'
+    argError = true;
+  } else if ( ! req.body.args.dimensions ) {
+    response.status = 400;
+    response.body = 'The args must include an array of "dimensions" which defines the width and height of each dimension'
+    argError = true;
+  }
+
+  if ( argError ) {
+    context.res = response;
+    context.done();
+  }
+
+  context.bindings.msg = req.body;
+  context.res = {
+    body: "Successfully enqueued the request"
   }
 }
